4c1d576cca8a32111a67b75f3df70d1b0d516302
long2102 <phamhoanglong2102@gmail.com>
2021-04-08 15:41:13 +0700

    "Entities"

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -11,13 +11,23 @@ namespace BTL_Mvc
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+               name: "Auth",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Auth", action = "Login", id = UrlParameter.Optional }
+           );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+               name: "Admin",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+           );
         }
     }
 }
---
+++ Respository/ImpRespository/RespositoryBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTL_Mvc.Respository.ImpRespository
+{
+    public abstract class RespositoryBase<T> : IRespositoryBase<T> where T : class
+    {
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindByCondition(System.Linq.Expressions.Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
